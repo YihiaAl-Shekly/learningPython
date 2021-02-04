9d545e186ea188b12fba576852ca5dd3254fea49
Yihia <alshekly@outlook.com>
2021-02-04 16:17:00 -0500

    Create main.py
    test commit

---
+++ main.py
@@ -0,0 +1,9 @@
+wight = float(input("enter your wight"))
+print("is it in Kg (K) or Lbs (L)")
+kgOrLbs = input()
+if kgOrLbs == ('K' or 'k'):
+    print("your wight in lbs is " + str(wight * 2.205))
+
+elif kgOrLbs == ('l' or 'L'):
+    print("your wight in kg is " + str(wight / 2.205))
+print("done")
