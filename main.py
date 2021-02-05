0d80d49fc50a5f36b9c1e4da741bbcb7eb88160f
Yihia <alshekly@outlook.com>
2021-02-04 23:30:49 -0500

    Update main.py
    PID testing

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-wight = float(input("enter your wight"))
-print("is it in Kg (K) or Lbs (L)")
-kgOrLbs = input()
-if kgOrLbs == ('K' or 'k'):
-    print("your wight in lbs is " + str(wight * 2.205))
+# some PID math
 
-elif kgOrLbs == ('l' or 'L'):
-    print("your wight in kg is " + str(wight / 2.205))
-print("done")
-# test
+# the PID vals
+# proportional for the present error
+p = 2
+# integrator for the past error
+i = 1
+# derivative for the future error
+d = 0.1
+
+# the angle that i want the airplane to fly at
+intendedVal = float(50)
+# the angle that the airplane is actual flying at
+actualVal = float(0)
+
+error = intendedVal - actualVal
+oldError = error - 1
+time = float(1)
+# control surface control ( elevator for ex)
+pid_i = 0.0  # just for python not to be mad
+pid_p = error * p
+pid_i = pid_i + (i * error)
+pid_d = (d * oldError / time)
+elevatorAngle = pid_p + pid_i + pid_d
