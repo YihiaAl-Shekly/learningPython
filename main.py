8d067f7603c07c285eca2bf7b543093aefdf068e
Yihia <alshekly@outlook.com>
2021-02-04 23:52:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,24 @@
 
 # the PID vals
 # proportional for the present error
-p = 2
+p = 8
 # integrator for the past error
-i = 1
+i = 0.1
 # derivative for the future error
-d = 0.1
+d = 15
 
 # the angle that i want the airplane to fly at
-intendedVal = float(50)
+intendedVal = float(50)  # from input
 # the angle that the airplane is actual flying at
-actualVal = float(0)
+actualVal = float(0)  # from input
 
 error = intendedVal - actualVal
-oldError = error - 1
-time = float(1)
+oldError = error - 1  # idk how to calculate it yet
+time = float(1)  # need to be calculated
 # control surface control ( elevator for ex)
 pid_i = 0.0  # just for python not to be mad
+
+# the pid math
 pid_p = error * p
 pid_i = pid_i + (i * error)
 pid_d = (d * oldError / time)
